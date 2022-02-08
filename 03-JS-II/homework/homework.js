25404c43f5be8f5604a437e1aaaabda3113e81d7
Larryc8 <yeisonlarry1954@gmail.com>
2022-02-08 12:08:45 -0500

    primes fixed

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -68,17 +68,17 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  if(idioma === "orange"){
+  if(color === "orange"){
     return "This is orange";
   }
-  if(idioma === "green"){
+  if(color === "green"){
     return "This is green";
   }
-  if(idioma === "red"){
+  if(color === "red"){
     return "This is red";
   }
-  if(idioma === "blue"){
-    return "This is orange";
+  if(color === "blue"){
+    return "This is blue";
   }
 
   return "Color not found";
@@ -119,7 +119,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero%3 > 0 && numero%5 > 0) return numero;
+
   var valor = '';
+
   if(numero%3 === 0){
     valor = "fizz";
   }
@@ -136,14 +139,16 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-  if(num3===0 || num3===0 || num3===0) return "Error";
+  if(num1===0 || num2===0 || num3===0) return "Error";
+
+  if( num1<0 || num2<0 || num3<0 ){
+    return "Hay negativos";
+  }
 
   if( num1>num2 && num1>num3 && num1>0 ){
     return "Número 1 es mayor y positivo";
   }
-  if( num1<0 || num2<0 || num3<0 ){
-    return "Hay negativos";
-  }
+
   if( num3>num1 && num3>num2 ){
     return num3+1
   }
@@ -157,13 +162,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(nuemro === 1) return false;
+  let n = 0;
 
-  for(let i=2; i< 200; i++){
-    if(numero%i === 0) return false;
+  for(let i=1; i< 2000; i++){
+    if(numero%i === 0){
+      n++;
+    }
   }
+  if(n === 2) return true;
 
-  return true;
+  return false;
 }
 
 function esVerdadero(valor){
@@ -182,9 +190,9 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-  var mutl = [];
+  var mult = [];
 
-  for(let i=0; i<=60; i++){
+  for(let i=0; i<=10; i++){
     mult.push(6*i);
   }
   return mult;
@@ -194,11 +202,11 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero/100 < 1){
-    return false;
+  if(numero/100 >= 1 && numero/100<10 ){
+    return true;
   }
 
-  return true;
+  return false;
 
 }
 
